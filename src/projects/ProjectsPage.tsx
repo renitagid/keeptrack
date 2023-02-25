6c97e621dd5befcaef077409cce5e897ced82261
renitagid <renita.y.ip@gmail.com>
2023-02-24 16:05:14 -0800

    fixed useEffect to handle filtered projects

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -28,24 +28,24 @@ function ProjectsPage() {
     dispatch<any>(loadProjects(1));
   }, [dispatch]);
 
+  useEffect(() => {
+    setFilteredProjects(projects);
+  }, [projects]);
+
   const handleMoreClick = () => {
     dispatch(loadProjects(currentPage + 1));
   };
 
-const [filteredProjects, setFilteredProjects] = useState(projects);
-const filter = (projects: Project[], params: string) => {
-  const filteredProjects = projects.filter((project) => {
-    return (
-      project.name.toLowerCase().includes(params.toLowerCase()) ||
-      project.description
-        .toLowerCase()
-        .includes(params.toLowerCase())
-    );
-  });
-  setFilteredProjects(filteredProjects);
-}
-
-
+  const [filteredProjects, setFilteredProjects] = useState(projects);
+  const filter = (projects: Project[], params: string) => {
+    const filteredProjects = projects?.filter((project) => {
+      return (
+        project.name.toLowerCase().includes(params.toLowerCase()) ||
+        project.description.toLowerCase().includes(params.toLowerCase())
+      );
+    });
+    setFilteredProjects(filteredProjects);
+  };
 
   const [view, setView] = useState("table");
   const [tableButtonColor, setTableButtonColor] = useState("#b7c9e2");
@@ -53,32 +53,65 @@ const filter = (projects: Project[], params: string) => {
   const handleViewClick = (view: string): void => {
     setView(view);
     if (view === "table") {
-    setTableButtonColor("#b7c9e2");
-    setCardButtonColor("white");
+      setTableButtonColor("#b7c9e2");
+      setCardButtonColor("white");
     } else if (view === "card") {
       setTableButtonColor("white");
       setCardButtonColor("#b7c9e2");
     }
   };
-
-const handleChange = (e: { target: { value: any; }; }) => {
-    const { value } = e.target;
-    filter(projects, value);
-}
-
+  const [search, setSearch] = useState("");
+  const handleChange = (e: { target: { value: string } }) => {
+    setSearch(e.target.value);
+  };
+  const handleSearchClick = () => {
+    filter(projects, search);
+  };
   return (
     <div>
-      <div style={{ display: "flex", alignItems:"center",  justifyContent:"space-around"}}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
         <h1
           style={{ fontFamily: "Gloock, serif", padding: 4, color: "#01579b" }}
         >
           Current Projects
         </h1>
-        <button onClick={() => handleViewClick("table")} style={{backgroundColor:`${tableButtonColor}`, fontFamily:"Jost, sans-serif"}}>Table View</button>
-        <button onClick={() => handleViewClick("card")} style={{backgroundColor:`${cardButtonColor}`, fontFamily:"Jost, sans-serif"}}>Card View</button>
-        <div style={{fontFamily:"Jost, sans-serif" }}>Search:</div>
-        <input style={{fontFamily:"Jost, sans-serif" }} onChange={handleChange}></input>
-        <div style={{fontFamily:"Jost, sans-serif" }}>Results: {filteredProjects.length}</div>
+        <div>
+          <button
+            onClick={() => handleViewClick("table")}
+            style={{
+              backgroundColor: `${tableButtonColor}`,
+              fontFamily: "Jost, sans-serif",
+            }}
+          >
+            Table View
+          </button>
+          <button
+            onClick={() => handleViewClick("card")}
+            style={{
+              backgroundColor: `${cardButtonColor}`,
+              fontFamily: "Jost, sans-serif",
+            }}
+          >
+            Card View
+          </button>
+        </div>
+        <div
+          style={{ display: "flex", alignItems: "center", marginRight: "20px" }}
+        >
+          <input
+            style={{ fontFamily: "Jost, sans-serif" }}
+            onChange={handleChange}
+          ></input>
+          <button
+            onClick={handleSearchClick}
+            style={{ fontFamily: "Jost, sans-serif" }}
+          >
+            Search
+          </button>
+          <div style={{ fontFamily: "Jost, sans-serif" }}>
+            Results: {filteredProjects.length}
+          </div>
+        </div>
       </div>
       {error && (
         <div className="row">
@@ -92,8 +125,12 @@ const handleChange = (e: { target: { value: any; }; }) => {
           </div>
         </div>
       )}
-      {view==="table" && <ProjectTable projects={filteredProjects} />}
-      {view==="card" && <ProjectList projects={filteredProjects} />}
+      {!loading && view === "table" && (
+        <ProjectTable projects={filteredProjects} />
+      )}
+      {!loading && view === "card" && (
+        <ProjectList projects={filteredProjects} />
+      )}
 
       {!loading && !error && (
         <div className="row">
